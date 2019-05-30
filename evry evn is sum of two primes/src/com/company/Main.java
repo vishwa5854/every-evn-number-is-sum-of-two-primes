0113434f53663aece02c51cf1359c8ffcb24589e
Vishwanath Bondugula <40670894+vishwa5854@users.noreply.github.com>
2019-05-30 08:23:38 +0530

    Add files via upload

---
+++ evry evn is sum of two primes/src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Writer writer = new FileWriter("d:\\Test\\prime_sumeven.txt");
+        for(long i = 2; true; i++){
+            logic(2*i,writer);
+        }
+    }
+
+    private static void logic(long input,Writer writer) throws IOException {
+        int count=0;
+        writer.append("For input :" + input + "{ \n");
+        for(long i=2;i<input;i++){
+            if((checkIfPrime(i)) && (checkIfPrime(input-i))){
+                count++;
+                //System.out.println(i + (input - i));
+                String te = Long.toString(i);
+                te += ",";
+                te += Long.toString(input-i);
+                te += "\n";
+                writer.append("\t" + te);
+            }
+        }
+        if(count == 0){
+            System.out.println("For" + input + "not satisfied");
+            writer.close();
+        }
+        writer.append("}");
+    }
+
+    private static boolean checkIfPrime(long n){
+        for(long i=2;i<n;i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
